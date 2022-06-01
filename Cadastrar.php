c428dcafd68481a98482b0d7e77e07ae8d5bcae7
Niiicolly <101739320+Niiicolly@users.noreply.github.com>
2022-06-01 20:53:15 -0300

    Update Cadastrar.php

--- Cadastrar.php
+++ Cadastrar.php
@@ -1,1 +1,11 @@
-Testaa
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tela de Login</title>
+</head>
+<body>
+	<input type="text" name="login" placeholder="Nome de Usuário">
+	<input type="password" name="senha"´placeholder="Senha">
+	<button>Enviar</button>
+</body>
+</html>
