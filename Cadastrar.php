bc39660342d40e1ae76de4cd6c93333aaf4b845e
ev1nha <106712510+ev1nha@users.noreply.github.com>
2022-06-01 21:33:45 -0300

    Update Cadastrar.php

--- Cadastrar.php
+++ Cadastrar.php
@@ -7,6 +7,7 @@
 	<h1> Bem - vindo a sua tela de login </h1>
 	<h2> Insira as suas informações </h2>
 	<input type="text" name="login" placeholder="Nome de Usuário">
+	<p/>
 	<input type="password" name="senha" placeholder="Senha">
 	<button> Enviar </button>
 </body>
