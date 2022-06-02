66ae90aff0b6a85159950438e100c239e76535b6
Niiicolly <101739320+Niiicolly@users.noreply.github.com>
2022-06-01 21:24:10 -0300

    Update Cadastrar.php

--- Cadastrar.php
+++ Cadastrar.php
@@ -7,7 +7,7 @@
 	<h1> Bem - vindo a sua tela de login </h1>
 	<h2> Insira as suas informações </h2>
 	<input type="text" name="login" placeholder="Nome de Usuário">
-	<input type="password" name="senha"´placeholder="Senha">
+	<input type="password" name="senha" placeholder="Senha">
 	<button> Enviar </button>
 </body>
 </html>
