6b4c66780abe88d064f16fa4b2a285a61349e972
ev1nha <106712510+ev1nha@users.noreply.github.com>
2022-06-01 21:59:16 -0300

    Update As Branquelas.php

--- As Branquelas.php
+++ As Branquelas.php
@@ -6,7 +6,15 @@
 <body>
 	<h1> Informações sobre o filme escolhido  </h1>
   <img php/asbranq.jpg> 
-	<h2>  </h2>
+	<p/>
+	<h3> 14 anos 109 min. Comédia EUA 2004 Keenen Ivory Wayans
+TÍTULO ORIGINAL: (White Chicks)
+
+IDIOMA: Inglês LEGENDA: Português
+ELENCO: Shawn Wayans, Marlon Wayans, Maitland Ward e Lochlyn Munro </h3>
+	<h2> Sinopse </h2>
+	<h3> Dois irmãos agenstes do FBI, Marcus e Kevin, acidentalmente evitam que bandidos sejam presos em uma apreensão de drogas. Como castigos, eles são obrigados a escoltar um par de socialites para o Hampton.
+		Porém quando as meninas descobrem o plano do FBI, elas se recusam a ir. Sem opções, Marcus e Kevin de cidem posar como irmãs, transformando - se em um par de loiras. </h3>
 
 </body>
 </html>
