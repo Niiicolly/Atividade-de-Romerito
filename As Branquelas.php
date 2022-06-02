bc401975d31ac464efac57217f4b9f7a74ef9010
ev1nha <106712510+ev1nha@users.noreply.github.com>
2022-06-01 21:48:52 -0300

    Update As Branquelas.php

--- As Branquelas.php
+++ As Branquelas.php
@@ -1,1 +1,12 @@
-Filme de Evelyn
+<!DOCTYPE html>
+<html>
+<head>
+	<title> AS BRANQUELAS </title>
+</head>
+<body>
+	<h1> Informações sobre o filme escolhido  </h1>
+  <img php/asbranq.jpg> 
+	<h2>  </h2>
+
+</body>
+</html>
